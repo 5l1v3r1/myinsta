1c84ed322bc76258cf6342d06110fd1c6b8bcbea
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-12-22 00:25:50 +0530

    Update S.sh

--- S.sh
+++ S.sh
@@ -1,3 +1,4 @@
-python2
-import os
-os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
+echo """python2
+echo """
+echo """import os
+echo """os.system('xdg-open https://www.instagram.com/shubham_g0sain/?hl=en')
